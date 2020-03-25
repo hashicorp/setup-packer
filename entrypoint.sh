030c38a4f110885ce34305dce1deb3a65e8e1abf
Kerim Satirli <k.satirli@gmail.com>
2020-03-25 13:11:08 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-# make sure INPUT_COMMAND is set
+# fail if INPUT_COMMAND is not set
 if [ -z "${INPUT_COMMAND}" ]; then
-  echo "Required variable `command` is missing"
+  echo "Required variable \`command\` is missing"
   exit 1
 fi
 
+# assemble operation
 if [ -z "${INPUT_ARGUMENTS}" ]; then
-  OPERATION="${INPUT_COMMAND} ${INPUT_ARGUMENTS} ${INPUT_TARGET}"
+  OPERATION="packer ${INPUT_COMMAND} ${INPUT_ARGUMENTS} ${INPUT_TARGET}"
 else
-  OPERATION="${INPUT_COMMAND} ${INPUT_TARGET}"
+  OPERATION="packer ${INPUT_COMMAND} ${INPUT_TARGET}"
 fi
 
+echo "::debug:: name=command::${OPERATION}"
+
 # shellcheck disable=SC2086
-packer ${OPERATION}
+${OPERATION}
