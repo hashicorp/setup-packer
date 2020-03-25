d45334997f7d20cca124f1e6ed278bc01bf18b4c
Kerim Satirli <k.satirli@gmail.com>
2020-03-25 12:25:04 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-echo "env is"
-env
+# make sure INPUT_COMMAND is set
+if [ -z "${INPUT_COMMAND}" ]; then
+  echo "Required variable `command` is missing"
+  exit 1
+fi
 
-which packer
+if [ -z "${INPUT_ARGUMENTS}" ]; then
+  OPERATION="${INPUT_COMMAND} ${INPUT_ARGUMENTS} ${INPUT_TARGET}"
+else
+  OPERATION="${INPUT_COMMAND} ${INPUT_TARGET}"
+fi
+
+# shellcheck disable=SC2086
+packer ${OPERATION}
