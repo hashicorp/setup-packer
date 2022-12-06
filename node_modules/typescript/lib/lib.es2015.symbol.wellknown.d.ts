8c2e4ea0fc9cded0408636868b96f9c3e9e7c32a
Jenna Goldstrich <jenna.goldstrich@hashicorp.com>
2022-12-06 12:25:46 -0800

    Node attempt

--- node_modules/typescript/lib/lib.es2015.symbol.wellknown.d.ts
+++ node_modules/typescript/lib/lib.es2015.symbol.wellknown.d.ts
@@ -239,9 +239,9 @@ interface String {
     match(matcher: { [Symbol.match](string: string): RegExpMatchArray | null; }): RegExpMatchArray | null;
 
     /**
-     * Passes a string and {@linkcode replaceValue} to the `[Symbol.replace]` method on {@linkcode searchValue}. This method is expected to implement its own replacement algorithm.
-     * @param searchValue An object that supports searching for and replacing matches within a string.
-     * @param replaceValue The replacement text.
+     * Replaces first match with string or all matches with RegExp.
+     * @param searchValue A string or RegExp search value.
+     * @param replaceValue A string containing the text to replace for match.
      */
     replace(searchValue: { [Symbol.replace](string: string, replaceValue: string): string; }, replaceValue: string): string;
 
--- node_modules/typescript/lib/lib.es2019.d.ts
+++ node_modules/typescript/lib/lib.es2019.d.ts
@@ -23,4 +23,3 @@ and limitations under the License.
 /// <reference lib="es2019.object" />
 /// <reference lib="es2019.string" />
 /// <reference lib="es2019.symbol" />
-/// <reference lib="es2019.intl" />
